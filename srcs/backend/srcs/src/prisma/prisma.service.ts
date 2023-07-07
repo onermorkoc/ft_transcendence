806262d157d3ba8e5dab62c17399cbfd80d6ceeb
onermorkoc <36oner75@gmail.com>
2023-07-07 21:40:06 +0300

    Cum 07 Tem 2023 21:40:06 +03

--- srcs/backend/srcs/src/prisma/prisma.service.ts
+++ srcs/backend/srcs/src/prisma/prisma.service.ts
@@ -7,9 +7,9 @@ export class PrismaService extends PrismaClient {
         super({
             datasources: {
                 db: {
-                    url: 'postgresql://postgres:postgres@localhost:5432/postgres?schema=public'
+                    url: "postgresql://postgres:postgres@database:5432/postgres?schema=public"
                 }
             }
-        })
+        });
     }
 }
